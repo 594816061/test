9f4153fac47b5a0502973f470871b0b8a27abd8b
wsh <594816061@qq.com>
2019-06-04 17:00:27 +0800

    ORM作业

--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+# 继承models.Model可以拥有父类的功能~操作数据库
+class BookInfo(models.Model):
+    """图书类"""
+    title = models.CharField(max_length=20)
+    pub_date = models.DateTimeField()
+
+
+class HeroInfo(models.Model):
+    """英雄类"""
+    name = models.CharField(max_length=20)
+    gender = models.BooleanField(default=True)
+    content = models.CharField(max_length=100)
+    # book作为外键关联到表
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
