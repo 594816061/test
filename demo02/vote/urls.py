83e306e5108823a1a6ba20e4603d2fb37290bd18
wsh <594816061@qq.com>
2019-06-18 15:16:14 +0800

    依赖

--- demo02/vote/urls.py
+++ demo02/vote/urls.py
@@ -1,7 +1,6 @@
 """应用路由"""
 from django.conf.urls import url
-from .views import index, detail, result, login, regist, my_logout, check_my_username, my_active
-
+from .views import index, detail, result, login, regist, my_logout, check_my_username, my_active, verify
 app_name = 'vote'
 
 urlpatterns = [
@@ -13,4 +12,5 @@ urlpatterns = [
     url(r'^my_logout/$', my_logout, name='my_logout'),
     url(r'^check_my_username/$', check_my_username, name='check_my_username'),
     url(r'^my_active/(.*?)/$', my_active, name='my_active'),
+    url(r'^verify/$', verify, name='verify'),
 ]
--- demo02/vote/views.py
+++ demo02/vote/views.py
@@ -7,6 +7,9 @@ from django.contrib import auth
 from django.core.mail import send_mail,EmailMultiAlternatives
 from demo02 import settings
 from itsdangerous import TimedJSONWebSignatureSerializer
+from PIL import Image, ImageDraw, ImageFont
+import random,io
+from django.core.cache import cache
 
 
 # Create your views here.
@@ -67,12 +70,18 @@ def login(req):
             # username = lf.cleaned_data['username']
             username = req.POST.get('username')
             password = req.POST.get('password')
+            verifycode = req.POST.get('verifycode')
             # id = req.POST.get('id')
             user = auth.authenticate(req, username=username, password=password)
             if user:
                 # 登录成功
-                auth.login(req, user)
-                return redirect(reverse('vote:index'))
+                if verifycode == req.session["verifycode"]:     # cache.get('verifycode')
+                    auth.login(req, user)
+                    return redirect(reverse('vote:index'))
+                else:
+                    errormessage = '验证码输入错误'
+                    return render(req, 'test/login.html', locals())
+
             else:
                 rf = MyUserRegistForm()
                 lf = MyUserLoginForm()
@@ -126,12 +135,13 @@ def my_logout(req):
 
 
 def check_my_username(req):
-    username = req.GET.get('username')
-    user = MyUser.objects.filter(username=username).first()
-    if user:
-        return JsonResponse({'statecode':'1'})
-    else:
-        return JsonResponse({'statecode':'0', 'error':'用户名不存在'})
+    if req.method == "GET":
+        username = req.GET.get('username')
+        user = MyUser.objects.filter(username=username).first()
+        if user:
+            return JsonResponse({'statecode':'1'})
+        else:
+            return JsonResponse({'statecode':'0', 'error':'用户名不存在'})
 
 
 def my_active(req, id):
@@ -144,4 +154,42 @@ def my_active(req, id):
     return redirect(reverse('vote:login'))
 
 
-
+def verify(request):
+    if request.method == "GET":
+    # 定义变量， 用于画面的背景色、 宽、 高
+        bgcolor = (random.randrange(20,100),
+        random.randrange(20,100),
+        random.randrange(20,100))
+        width = 100
+        heigth = 35
+        # 创建画面对象
+        im = Image.new('RGB',(width,heigth),bgcolor)
+        # 创建画笔对象
+        draw = ImageDraw.Draw(im)
+        # 调用画笔的point()函数绘制噪点
+        for i in range(0, 100):
+            xy = (random.randrange(0, width), random.randrange(0, heigth))
+            fill = (random.randrange(0, 255), 255, random.randrange(0, 255))
+            draw.point(xy, fill=fill)
+        # 定义验证码的备选值
+        str1 = 'ABCD123EFGHIJK456LMNOPQRS789TUVWXYZ0'
+        # 随机选取4个值作为验证码
+        rand_str = ''
+        for i in range(0, 4):
+            rand_str += str1[random.randrange(0, len(str1))]
+        # 构造字体对象
+        font = ImageFont.truetype('calibrili.ttf', 23)
+        fontcolor = (255, random.randrange(0, 255), random.randrange(0, 255))
+        # 绘制4个字
+        draw.text((5, 2), rand_str[0], font=font, fill=fontcolor)
+        draw.text((25, 2), rand_str[1], font=font, fill=fontcolor)
+        draw.text((50, 2), rand_str[2], font=font, fill=fontcolor)
+        draw.text((75, 2), rand_str[3], font=font, fill=fontcolor)
+        # 释放画笔
+        del draw
+        request.session['verifycode'] = rand_str
+        # cache.set('verifycode', rand_str, 300)
+        f = io.BytesIO()
+        im.save(f,'png')
+        # 将内存中的图片数据返回给客户端， MIME类型为图片png
+        return HttpResponse(f.getvalue(), 'image/png')
